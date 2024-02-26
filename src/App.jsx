41d386a73b76b16b2ef63ad700729e752242221c
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-02-26 11:23:27 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import "./App.css";
 import WeatherInfo from "./components/WeatherInfo";
 import SearchWeather from "./components/SearchWeather";
 
-
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+  const [cityName, setCityName] = useState("");
 
-	const [weatherData, setWeatherData] = useState({});
-	const [cityName, setCityName] = useState('');
-
-  	return (
-		<div>
-			{
-				weatherData.isRendered ? <WeatherInfo weatherData={weatherData} />
-				: <SearchWeather weatherData={weatherData} setWeatherData={setWeatherData} cityName={cityName} setCityName={setCityName} />
-			}
-		</div>
-			
-	)
+  return (
+    <div>
+      {weatherData.isRendered ? (
+        <WeatherInfo weatherData={weatherData} />
+      ) : (
+        <SearchWeather
+          weatherData={weatherData}
+          setWeatherData={setWeatherData}
+          cityName={cityName}
+          setCityName={setCityName}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -2,20 +2,17 @@ import React from "react";
 import LocationDate from "./WeatherInfoComponents/LocationDate";
 import CurrentTemp from "./WeatherInfoComponents/CurrentTemp";
 import CurrentStatus from "./WeatherInfoComponents/CurrentStatus";
-import ForecastHourly from "./WeatherInfoComponents/ForecastHourly";
 import ForecastDaily from "./WeatherInfoComponents/ForecastDaily";
 
 function WeatherInfo({ weatherData }) {
-
-	return (
-		<main className="main-container">
-			<LocationDate weatherData={weatherData} />
-			<CurrentTemp weatherData={weatherData} />
-			<CurrentStatus weatherData={weatherData} />
-			{/*<ForecastHourly weatherData={weatherData} />
-			<ForecastDaily weatherData={weatherData} />*/}
-		</main>
-	);
+  return (
+    <main className="main-container">
+      <LocationDate weatherData={weatherData} />
+      <CurrentTemp weatherData={weatherData} />
+      <CurrentStatus weatherData={weatherData} />
+      <ForecastDaily weatherData={weatherData} />
+    </main>
+  );
 }
 
 export default WeatherInfo;
